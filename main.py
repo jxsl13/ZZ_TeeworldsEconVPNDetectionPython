17f6f41e2b7bf78bc419813497085a450a0beb31
John Behm <jbehm@uos.de>
2019-10-13 16:59:07 +0200

    make use of all apis, when detecting vpns, >=50% should return a vpn response added logging added some redis db error handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import asyncio
+from datetime import datetime
 import os
 import re
 import telnetlib
@@ -9,6 +10,7 @@ from dotenv import load_dotenv
 import redis
 
 from vpn_apis import API_GetIPIntel_Net, API_IP_Teoh_IO, API_IPHub
+
 
 
 def execute(conn: telnetlib.Telnet, line: str) -> None:
@@ -34,6 +36,13 @@ def login(conn: telnetlib.Telnet, password: str) -> bool:
     else:
         return False
 
+def log(conn: telnetlib.Telnet, level: str, msg : str):
+    now = datetime.now()
+    timestamp = now.strftime("%Y.%m.%d %H:%M:%S")
+    line = f"[{timestamp}][{level}]: {msg}"
+    print(line)
+    execute(conn, f"echo {line}")
+
 def get_ip_id(line: str) -> (str, int):
     pattern = r"ClientID=([\d]{1,2}) addr=((?:(?:1\d\d|2[0-5][0-5]|2[0-4]\d|0?[1-9]\d|0?0?\d)\.){3}(?:1\d\d|2[0-5][0-5]|2[0-4]\d|0?[1-9]\d|0?0?\d))"
     match = re.search(pattern, line)
@@ -41,6 +50,16 @@ def get_ip_id(line: str) -> (str, int):
         return match.group(2), match.group(1)
     else:
         return None, None
+
+def decide_is_vpn(conn: telnetlib.Telnet, api_results: dict, ip: str) -> bool:
+    get_ip_intel = api_results['GetIPIntel']
+    ip_hub = api_results['IPHub']
+    ip_theo = api_results['IPTheo']
+
+    true_count = len([value for key, value in api_results.items() if value == True])
+    total_count = len(api_results)
+    log(conn, "VPN", f"IP: {ip} - GetIPIntel: {int(get_ip_intel)} IPHub: {int(ip_hub)} IPTheo: {int(ip_theo)}")
+    return (true_count / total_count) > 0.5
 
 
 async def main():
@@ -57,13 +76,17 @@ async def main():
     vpn_ban_reason = os.getenv("VPN_BAN_REASON")
 
     # database
-    r = redis.Redis()
+    try:
+        r = redis.Redis(socket_connect_timeout=1)
+        r.ping()
+    except:
+        r = None
 
     # econ connection
     conn = telnetlib.Telnet()
     conn.open(econ_host, econ_port)
 
-    vpn_apis = [API_GetIPIntel_Net(email, 0.95), API_IPHub(iphub_token), API_IP_Teoh_IO()]
+    vpn_apis = [("GetIPIntel", API_GetIPIntel_Net(email, 0.95)), ("IPHub", API_IPHub(iphub_token)), ("IPTheo", API_IP_Teoh_IO())]
 
     if login(conn, password):
 
@@ -74,39 +97,50 @@ async def main():
                 ip, client_id = get_ip_id(line)
                 if ip:
                     # init values
+                    exists = None
+                    is_vpn_response_dict = {}
                     is_vpn = False
                     got_response = False
-
-                    exists = r.get(ip)
+                    
+                    if r != None:
+                        exists = r.get(ip)
+                    
 
                     if exists == None:
                         # does not exist in redis database
                         # needs to be retrieved from api endpoints
 
-                        for api in vpn_apis:
+                        for key, api in vpn_apis:
                             err, is_vpn = await api.is_vpn(ip)
                 
                             if err:
+                                is_vpn_response_dict[key] = None
                                 continue
                             else:
                                 got_response = True
-                                if is_vpn:
-                                    break
+                                is_vpn_response_dict[key] = is_vpn
+
                         
                         if got_response:
-                            if is_vpn:
-                                # vpns are being kept forever
-                                r.set(ip, int(is_vpn))
-                            else:
-                                # non vpn ips are being kept for one week, before they are
-                                # checked again.
-                                r.set(ip, int(is_vpn), ex=(3600 * 24 * 7))
+                            # decide based on all three results
+                            is_vpn = decide_is_vpn(conn, is_vpn_response_dict, ip)
+
+                            # set values in redis database
+                            if r != None:
+                                if is_vpn:
+                                    # vpns are being kept forever
+                                    r.set(ip, int(is_vpn))
+                                else:
+                                    # non vpn ips are being kept for one week, before they are
+                                    # checked again.
+                                    r.set(ip, int(is_vpn), ex=(3600 * 24 * 7))
                         else:
                             # is not vpn, cuz could not retrieve data.
                             pass
                     else:
                         # exists in db
                         is_vpn = bool(int(exists))
+                        log(conn, "VPN", f"IP: {ip} - In cache: {int(is_vpn)}")
                     
                     if is_vpn:
                         execute(conn, f'ban {client_id} {vpn_ban_time} "{vpn_ban_reason}"')
